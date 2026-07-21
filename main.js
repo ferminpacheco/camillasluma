c598f526410b47ee4d38cad7e1a9f8ff1b0268b3
talimaizel-dotcom <talimaizel@gmail.com>
2026-07-21 13:23:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ function cambiarColor(color) {
     const imagen = document.getElementById('img-luminaria');
     const dots = document.querySelectorAll('.color-dot');
 
-    // Cambiar la ruta de la imagen según el color elegido
     if (color === 'negro') {
         imagen.src = 'images/Luma_luminaria.png';
     } else if (color === 'blanco') {
         imagen.src = 'images/Luma_luminaria_blanca.png';
     }
 
-    // Actualizar visualmente qué círculo está seleccionado
     dots.forEach(dot => {
         dot.classList.remove('active');
         if (color === 'negro' && dot.classList.contains('black')) dot.classList.add('active');
@@ -21,13 +19,12 @@ function toggleMenu() {
     nav.classList.toggle('active');
 }
 
-// Cerrar el menú automáticamente al hacer clic en una opción (opcional)
 document.querySelectorAll('.main-nav a').forEach(link => {
     link.addEventListener('click', () => {
         document.querySelector('.main-nav').classList.remove('active');
     });
 });
-// Función para detectar el scroll y activar animaciones
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -35,18 +32,15 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 }, {
-    threshold: 0.1 // Se activa cuando se ve el 10% del bloque
+    threshold: 0.1 
 });
 
-// Seleccionamos todos los elementos que queremos animar
 document.querySelectorAll('.reveal, .reveal-left').forEach((el) => observer.observe(el));
-// Obtener el botón
 const topBtn = document.getElementById("backToTop");
 
-// Mostrar el botón cuando se baja 200px
+
 window.onscroll = function() {
     scrollFunction();
-    // Aquí podés mantener también la función de la barra de progreso que vimos antes
 };
 
 function scrollFunction() {
@@ -57,7 +51,6 @@ function scrollFunction() {
     }
 }
 
-// Función para volver arriba suavemente
 function scrollToTop() {
     window.scrollTo({
         top: 0,
